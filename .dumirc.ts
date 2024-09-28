e3c05db11433900d1fc3cebb3ca7b071dda7c28d
leilei405 <1601178425@qq.com>
2024-09-28 23:01:40 +0800

    feat: ✨dumirc 配置文件修改

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,19 +2,35 @@
  * @Author: leilei405 1601178425@qq.com
  * @Date: 2024-09-27 23:28:00
  * @LastEditors: leilei405 1601178425@qq.com
- * @LastEditTime: 2024-09-28 01:10:50
+ * @LastEditTime: 2024-09-28 15:07:38
  * @FilePath: \lucky-design-doc\.dumirc.ts
  */
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'lucky-design',
+    nav: [
+      { title: '介绍', link: '/guide' },
+      { title: '组件', link: '/components/Button' }, // components会默认自动对应到src文件夹
+    ],
+    sidebar: {},
   },
-  // 打开多语言入口
-  locales: [
-    { id: 'zh-CN', name: '中文简体' },
-    { id: 'en-US', name: 'English' },
+  styles: [
+    `.dumi-default-header-left {
+       width: 220px !important;
+    }`,
   ],
+  resolve: {
+    entryFile: './src/index.ts',
+  },
+  history: {
+    type: 'hash',
+  },
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+    '@docs': path.resolve(__dirname, 'docs'),
+  },
 });
